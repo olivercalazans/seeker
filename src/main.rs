6fddc0d9fce1dce8dbc522c76f0dbfddbc6feaa6
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-08-21 22:16:51 -0300

    main refactored for better understanding

--- src/main.rs
+++ src/main.rs
@@ -14,41 +14,96 @@ mod utils;
 
 
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
-    let all_commands          = get_commands();
-    let command:String        = args.remove(1);
+    let mut seeker = Command::new();
+    seeker.run();
+}
 
-    if let Some(cmd) = all_commands.get(&command){
-        cmd();
-    } else {
-        println!("ERROR: no command {}", command)
+
+
+#[derive(Default)]
+struct Command {
+    all_commands: HashMap<String, fn()>,
+    arguments: Vec<String>,
+    command: String,
+}
+
+
+impl Command {
+
+    pub fn new() -> Self {
+        Default::default()
     }
-}
-
-
-fn get_commands() -> HashMap<String, fn()> {
-    let mut commands:HashMap<String, fn()> = HashMap::new();
-    commands.insert("--help".to_string(), display_help);
-    commands.insert("netmap".to_string(), run_netmap);
-    return commands;
-}
-
-
-fn display_help() {
-    println!(
-        "> Seeker is a tool for network exploration\n\
-         > For more information visite the repository:\n\
-         > https://github.com/olivercalazans/seeker\n\
-         \n\
-         Available commands:
-    ");
-    println!("  - netmap: Network Mapping");
-    println!("OBS.: Use --help with the command to see more about them")
-}
 
 
 
-fn run_netmap() {
-    let netmapper = NetworkMapper::new();
-    netmapper.execute();
+    pub fn run(&mut self) {
+        self.validate_input();
+        self.get_command_list();
+        self.validate_command_name();
+    }
+
+
+
+    fn exit(error: impl Into<String>) -> ! {
+        eprintln!("Error: {}", error.into());
+        std::process::exit(1);
+    }
+
+
+
+    fn validate_input(&mut self) {
+        let mut input: Vec<String> = env::args().collect();
+        
+        if input.get(1).is_none() {
+            Self::exit("No input found");
+        }
+
+        self.command   = input.remove(1);
+        self.arguments = input;
+    }
+
+
+
+    fn get_command_list(&mut self) {
+        self.all_commands.insert("--help".to_string(), Self::display_help);
+        self.all_commands.insert("netmap".to_string(), Self::run_netmap);
+    }
+
+
+
+    fn validate_command_name(&self) {
+        if self.all_commands.get(&self.command).is_none(){
+            Self::exit(format!("no command '{}'", self.command))
+        }
+        
+        self.execute();
+    }
+
+
+
+    fn execute(&self) {
+        let cmd = self.all_commands.get(&self.command).unwrap();
+        cmd()
+    }
+
+
+    
+    fn display_help() {
+        println!(
+            "# Seeker is a tool for network exploration\n\
+             # For more information visite the repository:\n\
+             # https://github.com/olivercalazans/seeker\n\
+             \n\
+             Available commands:
+        ");
+        println!("  > netmap: Network Mapping");
+        println!("\nOBS.: Use --help with the command for more details")
+    }
+
+
+    
+    fn run_netmap() {
+        let netmapper = NetworkMapper::new();
+        netmapper.execute();
+    }
 }
