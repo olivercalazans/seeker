8c0b578265abad6ce5190589196fb40d8c535668
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-10-03 14:43:57 -0300

    portscan has a default port range

--- src/arg_parser/pscan_parser.rs
+++ src/arg_parser/pscan_parser.rs
@@ -13,8 +13,8 @@ pub struct PortScanArgs {
     /// Scan specific ports or ranges of ports (can be combined).
     ///
     /// Examples: Specific: 22,80 | Range: 20-50 | Combined: 22,50-100
-    #[arg(short, long)]
-    pub ports: Option<String>,
+    #[arg(short, long, default_value = "0-100")]
+    pub ports: String,
 
 
     /// Scan the ports in random order
