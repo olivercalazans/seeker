693a99aedc7230cd1a23b7e982052c49abba7320
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-10-24 12:27:59 -0300

    simplified buffers: now limited to three layers and a single packet

--- src/pkt_kit/pkt_builder.rs
+++ src/pkt_kit/pkt_builder.rs
@@ -2,14 +2,13 @@ use std::net::Ipv4Addr;
 use pnet::datalink::MacAddr;
 use pnet::packet::ip::IpNextHeaderProtocols as ProtoID;
 use rand::{Rng, rngs::ThreadRng};
-use crate::pkt_kit::{HeaderBuffer, PacketBuffer, HeaderBuilder};
-use crate::utils::{get_ipv4_addr};
+use crate::pkt_kit::{PacketBuffer, HeaderBuilder};
+use crate::utils::get_ipv4_addr;
 
 
 
 pub struct PacketBuilder {
-    headers: HeaderBuffer,
-    packets: PacketBuffer,
+    pkt_buf: PacketBuffer,
     src_ip:  Ipv4Addr,
     rng:     ThreadRng,
 }
@@ -20,8 +19,7 @@ impl PacketBuilder {
 
     pub fn new(iface: String, src_ip: Option<Ipv4Addr>) -> Self {
         Self {
-            headers: HeaderBuffer::default(),
-            packets: PacketBuffer::default(),
+            pkt_buf: PacketBuffer::default(),
             src_ip:  src_ip.unwrap_or_else(|| get_ipv4_addr(&iface)),
             rng:     rand::thread_rng(),
         }
@@ -34,14 +32,14 @@ impl PacketBuilder {
         let src_mac  = self.random_mac();
         let dst_mac  = self.random_mac();
 
-        HeaderBuilder::create_ether_header(&mut self.headers.ether, src_mac, dst_mac);
-        HeaderBuilder::create_ip_header(&mut self.headers.ip, 40, ProtoID::Tcp, src_ip, dst_ip);
-        HeaderBuilder::create_tcp_header(&mut self.headers.tcp, src_ip, src_port, dst_ip, 80);
+        HeaderBuilder::create_ether_header(&mut self.pkt_buf.ether, src_mac, dst_mac);
+        HeaderBuilder::create_ip_header(&mut self.pkt_buf.ip, 40, ProtoID::Tcp, src_ip, dst_ip);
+        HeaderBuilder::create_tcp_header(&mut self.pkt_buf.layer4, src_ip, src_port, dst_ip, 80);
         
-        self.packets.tcp_layer2[..14].copy_from_slice(&self.headers.ether);
-        self.packets.tcp_layer2[14..34].copy_from_slice(&self.headers.ip);
-        self.packets.tcp_layer2[34..].copy_from_slice(&self.headers.tcp);
-        &self.packets.tcp_layer2
+        self.pkt_buf.packet[..14].copy_from_slice(&self.pkt_buf.ether);
+        self.pkt_buf.packet[14..34].copy_from_slice(&self.pkt_buf.ip);
+        self.pkt_buf.packet[34..].copy_from_slice(&self.pkt_buf.layer4);
+        &self.pkt_buf.packet
     }
 
 
@@ -49,12 +47,12 @@ impl PacketBuilder {
     pub fn build_tcp_ip_packet(&mut self, dst_ip: Ipv4Addr, dst_port: u16) -> &[u8] {
         let src_port = self.rng.gen_range(10000..=65535);
 
-        HeaderBuilder::create_ip_header(&mut self.headers.ip, 40, ProtoID::Tcp, self.src_ip, dst_ip);
-        HeaderBuilder::create_tcp_header(&mut self.headers.tcp, self.src_ip, src_port, dst_ip, dst_port);
+        HeaderBuilder::create_ip_header(&mut self.pkt_buf.ip, 40, ProtoID::Tcp, self.src_ip, dst_ip);
+        HeaderBuilder::create_tcp_header(&mut self.pkt_buf.layer4, self.src_ip, src_port, dst_ip, dst_port);
         
-        self.packets.tcp_layer3[..20].copy_from_slice(&self.headers.ip);
-        self.packets.tcp_layer3[20..].copy_from_slice(&self.headers.tcp);
-        &self.packets.tcp_layer3
+        self.pkt_buf.packet[..20].copy_from_slice(&self.pkt_buf.ip);
+        self.pkt_buf.packet[20..40].copy_from_slice(&self.pkt_buf.layer4);
+        &self.pkt_buf.packet[..40]
     }
 
 
@@ -64,14 +62,14 @@ impl PacketBuilder {
         let src_mac  = self.random_mac();
         let dst_mac  = self.random_mac();
 
-        HeaderBuilder::create_ether_header(&mut self.headers.ether, src_mac, dst_mac);
-        HeaderBuilder::create_ip_header(&mut self.headers.ip, 28, ProtoID::Udp, src_ip, dst_ip);
-        HeaderBuilder::create_udp_header(&mut self.headers.udp, src_ip, src_port, dst_ip, 53);
+        HeaderBuilder::create_ether_header(&mut self.pkt_buf.ether, src_mac, dst_mac);
+        HeaderBuilder::create_ip_header(&mut self.pkt_buf.ip, 28, ProtoID::Udp, src_ip, dst_ip);
+        HeaderBuilder::create_udp_header(&mut self.pkt_buf.layer4, src_ip, src_port, dst_ip, 53);
 
-        self.packets.udp_layer2[..14].copy_from_slice(&self.headers.ether);
-        self.packets.udp_layer2[14..34].copy_from_slice(&self.headers.ip);
-        self.packets.udp_layer2[34..].copy_from_slice(&self.headers.udp);
-        &self.packets.udp_layer2
+        self.pkt_buf.packet[..14].copy_from_slice(&self.pkt_buf.ether);
+        self.pkt_buf.packet[14..34].copy_from_slice(&self.pkt_buf.ip);
+        self.pkt_buf.packet[34..42].copy_from_slice(&self.pkt_buf.layer4);
+        &self.pkt_buf.packet[..42]
     }
 
 
@@ -79,23 +77,23 @@ impl PacketBuilder {
     pub fn build_udp_ip_packet(&mut self, dst_ip: Ipv4Addr, dst_port: u16) -> &[u8] {
         let src_port = self.rng.gen_range(10000..=65535);
 
-        HeaderBuilder::create_ip_header(&mut self.headers.ip, 28, ProtoID::Udp, self.src_ip, dst_ip);
-        HeaderBuilder::create_udp_header(&mut self.headers.udp, self.src_ip, src_port, dst_ip, dst_port);
+        HeaderBuilder::create_ip_header(&mut self.pkt_buf.ip, 28, ProtoID::Udp, self.src_ip, dst_ip);
+        HeaderBuilder::create_udp_header(&mut self.pkt_buf.layer4, self.src_ip, src_port, dst_ip, dst_port);
 
-        self.packets.udp_layer3[..20].copy_from_slice(&self.headers.ip);
-        self.packets.udp_layer3[20..].copy_from_slice(&self.headers.udp);
-        &self.packets.udp_layer3
+        self.pkt_buf.packet[..20].copy_from_slice(&self.pkt_buf.ip);
+        self.pkt_buf.packet[20..28].copy_from_slice(&self.pkt_buf.layer4);
+        &self.pkt_buf.packet[..28]
     }
 
 
 
     pub fn build_icmp_echo_req_packet(&mut self, dst_ip: Ipv4Addr) -> &[u8] {
-        HeaderBuilder::create_ip_header(&mut self.headers.ip, 28, ProtoID::Icmp, self.src_ip, dst_ip);
-        HeaderBuilder::create_icmp_header(&mut self.headers.icmp);
+        HeaderBuilder::create_ip_header(&mut self.pkt_buf.ip, 28, ProtoID::Icmp, self.src_ip, dst_ip);
+        HeaderBuilder::create_icmp_header(&mut self.pkt_buf.layer4);
 
-        self.packets.icmp_layer3[..20].copy_from_slice(&self.headers.ip);
-        self.packets.icmp_layer3[20..].copy_from_slice(&self.headers.icmp);
-        &self.packets.icmp_layer3
+        self.pkt_buf.packet[..20].copy_from_slice(&self.pkt_buf.ip);
+        self.pkt_buf.packet[20..28].copy_from_slice(&self.pkt_buf.layer4);
+        &self.pkt_buf.packet[..28]
     }
 
 
