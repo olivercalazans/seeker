663b53095db8f787658afc59307d0231545aea57
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-10-27 20:05:51 -0300

    refactored src mac disscetor. Now it  uses slices

--- src/pkt_kit/pkt_dissector.rs
+++ src/pkt_kit/pkt_dissector.rs
@@ -1,4 +1,4 @@
-use etherparse::{SlicedPacket, InternetSlice, LinkSlice};
+use etherparse::{SlicedPacket, InternetSlice};
 
 
 
@@ -39,33 +39,16 @@ impl PacketDissector {
 
 
 
-    pub fn get_dst_ip(packet: &[u8]) -> String {
-        Self::get_headers(packet)
-            .and_then(|sliced| match sliced.net {
-                Some(InternetSlice::Ipv4(ipv4)) => {
-                    let [a, b, c, d] = ipv4.header().destination();
-                    Some(format!("{}.{}.{}.{}", a, b, c, d))
-                }
-                _ => None,
-            })
-            .unwrap_or_else(|| "unknown".to_string())
-    }
-
-
-
     pub fn get_src_mac(packet: &[u8]) -> String {
-        Self::get_headers(packet)
-            .and_then(|sliced| match sliced.link {
-                Some(LinkSlice::Ethernet2(eth)) => Some(
-                    eth.source()
-                        .iter()
-                        .map(|b| format!("{:02x}", b))
-                        .collect::<Vec<_>>()
-                        .join(":"),
-                ),
-                _ => None,
-            })
-            .unwrap_or_else(|| "unknown".to_string())
+        if packet.len() < 12 {
+            return "unknown".into();
+        }
+
+        packet[6..12]
+            .iter()
+            .map(|b| format!("{:02x}", b))
+            .collect::<Vec<_>>()
+            .join(":")
     }
-    
+
 }
