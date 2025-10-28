4ee77d92e0ce8dbdd75e70833fc1cd36f01a4e63
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-10-28 11:19:33 -0300

    crate dns-lookup removed

--- src/utils/dns.rs
+++ src/utils/dns.rs
@@ -1,18 +1,45 @@
-use std::net::IpAddr;
-use dns_lookup::lookup_addr;
+use std::ffi::CStr;
+use std::os::raw::c_char;
+use std::ptr;
+use libc::{addrinfo, freeaddrinfo, getnameinfo, getaddrinfo, NI_NAMEREQD, NI_MAXHOST, AF_UNSPEC, AI_NUMERICHOST};
 
 
 
 pub fn get_host_name(ip: &str) -> String {
-    let ip: IpAddr = ip.parse().unwrap();
-    match lookup_addr(&ip) {
-        Ok(hostname) => {
+    unsafe {
+        let mut hints: addrinfo = std::mem::zeroed();
+        hints.ai_family         = AF_UNSPEC;
+        hints.ai_flags          = AI_NUMERICHOST;
+
+        let mut res: *mut addrinfo = ptr::null_mut();
+
+        if getaddrinfo(ip.as_ptr() as *const i8, ptr::null(), &hints, &mut res) != 0 {
+            return "Unknown".to_string();
+        }
+
+        let mut host = [0 as c_char; NI_MAXHOST as usize];
+        let err = getnameinfo(
+            (*res).ai_addr,
+            (*res).ai_addrlen,
+            host.as_mut_ptr(),
+            NI_MAXHOST as u32,
+            ptr::null_mut(),
+            0,
+            NI_NAMEREQD,
+        );
+
+        freeaddrinfo(res);
+
+        if err == 0 {
+            let c_str    = CStr::from_ptr(host.as_ptr());
+            let hostname = c_str.to_string_lossy().into_owned();
             if hostname.ends_with(".lan") {
                 hostname.trim_end_matches(".lan").to_string()
             } else {
                 hostname
             }
+        } else {
+            "Unknown".to_string()
         }
-        Err(_) => "Unknown".to_string(),
     }
 }
